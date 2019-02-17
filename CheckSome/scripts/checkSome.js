cdd3811b26e7aafba0e2c03ec1fc5208858dc0ee
Jeong Won An <jw3an@uwaterloo.ca>
2019-02-16 22:55:50 -0500

    Added CheckSome

---
+++ CheckSome/scripts/checkSome.js
@@ -0,0 +1,252 @@
+$(document).ready(function () {
+  if (isNaN(localStorage["urlCounter"])) {
+    localStorage["urlCounter"] = 0;
+  }
+  renderURLList();
+});
+
+$('#addURLButton').click(function () {
+  $('#addStatus').html("<p>Adding... Do not make any changes.</p>");
+  var url = $('#urlInput').val();
+  var checkType = $('input[name=checkType]:checked').val();
+  if (!url.startsWith("http")) {
+    url = "http://" + url;
+  }
+  if (!$(`#${checkType}`).val()) {
+    $('#addStatus').html("<p>What should we check for?</p>");
+  }
+  else {
+    $.ajax({
+      url: url,
+      dataType: "html",
+      success: function (data) {
+        console.log("success!");
+        urlCounter = localStorage["urlCounter"];
+        localStorage["url" + urlCounter] = url;
+        console.log(localStorage["url" + urlCounter] + ' has been saved');
+        localStorage["urlCounter"] = parseInt(urlCounter) + 1;
+        var checkString;
+        localStorage["url" + urlCounter + "CheckType"] = checkType;
+        switch (checkType) {
+          case "exist":
+            localStorage["url" + urlCounter + "CheckTypeString"] = "exists.";
+            break;
+          case "notExist":
+            localStorage["url" + urlCounter + "CheckTypeString"] = "doesn't exist.";
+            break;
+          case "regex":
+            localStorage["url" + urlCounter + "CheckTypeString"] = "exists by regex.";
+            break;
+          default:
+            console.log("Unknown CheckType!");
+            break;
+        }
+        checkString = $(`#${checkType}`).val();
+        localStorage["url" + urlCounter + "CheckString"] = checkString;
+        localStorage["url" + urlCounter + "CheckStatus"] = "Unknown";
+        $('#addStatus').html("<p style='font-weight:bold; font-size: 150%;'>Added!</p>");
+        setTimeout(function () {
+          $('#addStatus').empty();
+        }, 2000);
+      },
+      error: function (e) {
+        console.log("Error: Not a valid URL.");
+        $('#addStatus').html("<p>Not a valid URL.</p>")
+      }
+    });
+  }
+});
+
+$('#urlList').on('click', 'button', function () {
+  var id = $(this).attr('id');
+  var urlNumber = $(this).attr('key');
+  if (id.endsWith("Delete")) {
+    console.log(urlNumber)
+    deleteURL(urlNumber);
+    renderURLList();
+  }
+  else {
+    $('#checkStatus' + urlNumber).html("<p>Checking...</p>");
+    $.ajax({
+      url: $(this).attr('id'),
+      dataType: "html",
+      success: function (data) {
+        console.log("success!");
+        console.log(data)
+        // var dataCheck = checkData(data, urlNumber);
+        // if (dataCheck) {
+        //   alert("True");
+        // }
+        // else {
+        //   alert("False");
+        // }
+
+        var dataCheck = existCheck(data, urlNumber);
+        if (dataCheck) {
+          localStorage["url" + urlNumber + "CheckStatus"] = "Exists!";
+          // alert("Exists!")
+        }
+        else {
+          localStorage["url" + urlNumber + "CheckStatus"] = "Does not exist!";
+          // alert("Does not exist!")
+        }
+        $('#checkStatus' + urlNumber).html(localStorage["url" + urlNumber + "CheckStatus"]);
+        setTimeout(function () {
+          $('#checkStatus' + urlNumber).empty();
+        }, 3000);
+        // if (parsedData != localStorage["url" + $(this).attr('key') + "Data"]) {
+        //   alert("Different!");
+        //   localStorage["url" + $(this).attr('key') + "Data"] = parsedData;
+        //   console.log(localStorage["url" + $(this).attr('key') + "Data"]);
+        // }
+        // else {
+        //   alert("Same");
+        // }
+      },
+      error: function (e) {
+        console.log("error loading");
+      }
+    });
+  }
+});
+
+$("#exist").keyup(function (event) {
+  if (event.keyCode == 13) {
+    $("#addURLButton").click();
+  }
+});
+$("#notExist").keyup(function (event) {
+  if (event.keyCode == 13) {
+    $("#addURLButton").click();
+  }
+});
+
+$('#exist').focus(function () {
+  $('#existRadio').prop("checked", true);
+  $('#notExistRadio').prop("checked", false);
+});
+
+$('#notExist').focus(function () {
+  $('#notExistRadio').prop("checked", true);
+  $('#existRadio').prop("checked", false);
+});
+
+$('#indexPage').click(function () {
+  window.location = "../Main/index.html"
+});
+
+$('#settingsPage').click(function () {
+  $('#settings').show();
+  $('#CheckSome').hide();
+  checkExpiryRange();
+});
+
+$('#homePage').click(function () {
+  $('#CheckSome').show();
+  $('#settings').hide();
+  renderURLList();
+});
+
+$('#addPage').click(function () {
+  $('#addURL').show();
+  $('#URLList').hide();
+});
+
+$('#showPage').click(function () {
+  $('#URLList').show();
+  $('#addURL').hide();
+  renderURLList();
+});
+
+$('#resetButton').click(function () {
+  localStorage["urlCounter"] = 0;
+  $('#deleteAllStatus').html("Deleted All.")
+});
+
+function removeElement(data, elementOpen, elementClose) {
+  var usefulData = data;
+  var parsedData = "";
+
+  while (true) {
+    var index = 0;
+    var elementStart = usefulData.search(elementOpen);
+    if (elementStart == -1) {
+      break;
+    }
+    parsedData += usefulData.substring(index, elementStart);
+    usefulData = usefulData.substring(elementStart);
+    index = usefulData.search(elementClose) + elementClose.length;
+    usefulData = usefulData.substring(index);
+  }
+  return parsedData;
+}
+
+// function parseData(data) {
+//   var parsedData = data;
+//   // parsedData = removeElement(parsedData, "\\?", "\\'");
+//   // parsedData = removeElement(parsedData, "<script>", "</script">");
+//   // parsedData = removeElement(parsedData, "<style>", "</style">");
+
+//   // based off of google.com results:
+//   // parsedData = removeElement(parsedData, "href=\"", "\"");
+//   // parsedData = removeElement(parsedData, "data-ved=\"", "\"");
+//   // parsedData = removeElement(parsedData, "onmousedown=\"", "\"");
+
+//   // parsedData = checkExists(parsedData);
+//   return parsedData;
+// }
+
+function existCheck(data, urlNumber) {
+  return (data.search(localStorage["url" + urlNumber + "CheckString"]) > 0);
+}
+
+function checkData(data, urlNumber) {
+  var dataCheck;
+  var checkType = localStorage["url" + urlNumber + "CheckType"];
+  switch (checkType) {
+    case "exist":
+      dataCheck = existCheck(data, urlNumber);
+      break;
+    case "notExist":
+      dataCheck = !existCheck(data, urlNumber);
+      break;
+    case "regex":
+      dataCheck = regexCheck(data, urlNumber);
+      break;
+    default:
+      console.log("Unknown CheckType!");
+      break;
+  }
+  return dataCheck;
+}
+
+function deleteURL(urlNumber) {
+  var urlCounter = parseInt(localStorage["urlCounter"]);
+  for (var i = parseInt(urlNumber); i < (urlCounter - 1); i++) {
+    localStorage["url" + i] = localStorage["url" + (i + 1)];
+    localStorage["url" + i + "CheckType"] = localStorage["url" + (i + 1) + "CheckType"];
+    localStorage["url" + i + "CheckString"] = localStorage["url" + (i + 1) + "CheckString"];
+    localStorage["url" + i + "CheckTypeString"] = localStorage["url" + (i + 1) + "CheckTypeString"];
+    localStorage["url" + i + "CheckStatus"] = localStorage["url" + (i + 1) + "CheckStatus"];
+  }
+  localStorage["urlCounter"] = urlCounter - 1;
+}
+
+function renderURLList() {
+  $('#urlList').empty();
+  for (var i = 0; i < parseInt(localStorage["urlCounter"]); i++) {
+    url = localStorage["url" + i];
+    $('#urlList').append("<div class='checkURL' id='checkURL" + i + "'></div>");
+    $('#checkURL' + i).append("<a id='openURL' target='_blank' href='" + url + "'>" + url + "</a>");
+    $('#checkURL' + i).append("<div class='checkInfo' id='checkInfo" + i + "'></div>");
+    $('#checkInfo' + i).append("<div class='checkStringInfo' id='checkStringInfo" + i + "'></div>");
+    $('#checkStringInfo' + i).append("Checks for string '" + localStorage["url" + i + "CheckString"] + "'<br>to see if it " + localStorage["url" + i + "CheckTypeString"]);
+    $('#checkInfo' + i).append("<div class='checkStatusInfo' id='checkStatusInfo" + i + "'></div>");
+    $('#checkStatusInfo' + i).append("<div id='checkStatus" + i + "'></div>");
+    $('#checkURL' + i).append("<button key=" + i + " id='" + url + "' class='btn btn-info'>Check!</button>");
+    $('#checkURL' + i).append("<button key=" + i + " id='" + url + "Delete' class='btn btn-danger buttons'>Delete</button>");
+  }
+  if (localStorage["urlCounter"] == 0) {
+    $('#urlList').html("<p>There are no URLs.</p>");
+  }
+}
--- ReMind/scripts/reMind.js
+++ ReMind/scripts/reMind.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   if (isNaN(localStorage["bookmarkCounter"])) {
     localStorage["bookmarkCounter"] = 0;
     localStorage["expiryRangeCounter"] = 0;
@@ -9,12 +9,12 @@ $(document).ready(function() {
   autoCurrent();
 });
 
-$('#notificationRangeInput').ready(function() {
+$('#notificationRangeInput').ready(function () {
   const notificationRange = localStorage["notificationRange"];
   $('#notificationRangeInput').val(notificationRange);
 });
 
-$('#addBookmarkButton').click(function() {
+$('#addBookmarkButton').click(function () {
   $('#addStatus').html("<p>Adding... Do not make any changes.</p>");
   let url = $('#urlInput').val();
   const title = $('#titleInput').val();
@@ -34,19 +34,19 @@ $('#addBookmarkButton').click(function() {
     localStorage["bookmarkCounter"] = parseInt(bookmarkCounter) + 1;
     $('#addStatus').html("<p style='font-weight:bold; font-size: 150%; text-align: center;'>Added!</p>");
   }
-  setTimeout(function() {
+  setTimeout(function () {
     $('#addStatus').empty();
   }, 2000);
   checkExpiryRange();
   renderBookmarkList();
 });
 
-$('#notificationRangeButton').click(function() {
+$('#notificationRangeButton').click(function () {
   localStorage["notificationRange"] = $('#notificationRangeInput').val();
 });
 
 //Deleting Bookmarks
-$('#bookmarkList').on('click', 'button', function() {
+$('#bookmarkList').on('click', 'button', function () {
   const id = $(this).attr('id');
   const bookmarkNumber = $(this).attr('key');
   if (id.endsWith("Delete")) {
@@ -55,54 +55,54 @@ $('#bookmarkList').on('click', 'button', function() {
   }
 });
 
-$("#urlInput").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#addBookmarkButton").click();
-    }
+$("#urlInput").keyup(function (event) {
+  if (event.keyCode == 13) {
+    $("#addBookmarkButton").click();
+  }
 });
 
-$("#remindDateInput").keyup(function(event){
-    if(event.keyCode == 13){
-        $("#addBookmarkButton").click();
-    }
+$("#remindDateInput").keyup(function (event) {
+  if (event.keyCode == 13) {
+    $("#addBookmarkButton").click();
+  }
 });
 
-$('#indexPage').click(function() {
+$('#indexPage').click(function () {
   window.location = "../Main/index.html"
 });
 
-$('#settingsPage').click(function() {
+$('#settingsPage').click(function () {
   $('#settings').show();
   $('#ReMind').hide();
   checkExpiryRange();
 });
 
-$('#addPage').click(function() {
-  $('#addBookmark').show();
-  $('#bookmarkList').hide();
-});
-
-$('#showPage').click(function() {
-  $('#bookmarkList').show();
-  $('#addBookmark').hide();
-});
-
-$('#homePage').click(function() {
+$('#homePage').click(function () {
   $('#ReMind').show();
   $('#settings').hide();
 });
 
-$('#resetButton').click(function() {
+$('#addPage').click(function () {
+  $('#addBookmark').show();
+  $('#bookmarkList').hide();
+});
+
+$('#showPage').click(function () {
+  $('#bookmarkList').show();
+  $('#addBookmark').hide();
+});
+
+$('#resetButton').click(function () {
   localStorage["bookmarkCounter"] = 0;
   $('#deleteAllStatus').html("Deleted All.")
-  chrome.browserAction.setBadgeText({text: ''});
+  chrome.browserAction.setBadgeText({ text: '' });
 });
 
 function deleteBookmark(bookmarkNumber) {
   const bookmarkCounter = parseInt(localStorage["bookmarkCounter"]);
-  for (let i = parseInt(bookmarkNumber); i < (bookmarkCounter-1); i++) {
-    localStorage["url" + i] = localStorage["url" + (i+1)];
-    localStorage["date" + i] = localStorage["date" + (i+1)];
+  for (let i = parseInt(bookmarkNumber); i < (bookmarkCounter - 1); i++) {
+    localStorage["url" + i] = localStorage["url" + (i + 1)];
+    localStorage["date" + i] = localStorage["date" + (i + 1)];
   }
   localStorage["bookmarkCounter"] = bookmarkCounter - 1;
   checkExpiryRange();
@@ -112,16 +112,16 @@ function renderBookmarkList() {
   $('#activeBookmarkList').empty();
   $('#expiredBookmarkList').empty();
   let currentDate = new Date();
-  currentDate.setDate(currentDate.getDate()-1);
-  for (let i=0; i<parseInt(localStorage["bookmarkCounter"]); i++) {
-    const url = localStorage["url"+i];
-    let date = parseIntoBigDate(localStorage["date"+i]);
+  currentDate.setDate(currentDate.getDate() - 1);
+  for (let i = 0; i < parseInt(localStorage["bookmarkCounter"]); i++) {
+    const url = localStorage["url" + i];
+    let date = parseIntoBigDate(localStorage["date" + i]);
     const list = currentDate < date ? "activeBookmarkList" : "expiredBookmarkList";
     date = parseIntoSmallDate(date);
-    $(`#${list}`).append("<div class='bookmark' id='bookmark"+i+"'></div>");
-    $('#bookmark'+i).append("<a id='openURL' target='_blank' href='"+url+"'>"+localStorage["title"+i]+"</a>");
-    $('#bookmark'+i).append("<div class='dateInfo' id='dateInfo"+i+"'>Expires: "+date+"</div>");
-    $('#bookmark'+i).append("<button key="+i+" id='"+url+"Delete' class='btn btn-danger buttons'>Delete</button>");
+    $(`#${list}`).append("<div class='bookmark' id='bookmark" + i + "'></div>");
+    $('#bookmark' + i).append("<a id='openURL' target='_blank' href='" + url + "'>" + localStorage["title" + i] + "</a>");
+    $('#bookmark' + i).append("<div class='dateInfo' id='dateInfo" + i + "'>Expires: " + date + "</div>");
+    $('#bookmark' + i).append("<button key=" + i + " id='" + url + "Delete' class='btn btn-danger buttons'>Delete</button>");
   }
   if ($('#activeBookmarkList').children().length === 0) {
     $('#activeBookmarkList').html("<p>There are no active bookmarks.</p>");
@@ -132,12 +132,12 @@ function renderBookmarkList() {
 }
 
 function autoCurrent() {
-  chrome.tabs.query({'active': true, 'currentWindow': true}, function(tabs) {
+  chrome.tabs.query({ 'active': true, 'currentWindow': true }, function (tabs) {
     $('#urlInput').val(tabs[0].url);
     $('#titleInput').val(tabs[0].title);
   });
   let currentDate = new Date();
-  let month = currentDate.getMonth()+1;
+  let month = currentDate.getMonth() + 1;
   if (month < 10) {
     month = "0" + month;
   }
@@ -146,7 +146,7 @@ function autoCurrent() {
 
 function checkDate(date) {
   let currentDate = new Date();
-  currentDate.setDate(currentDate.getDate()-1);
+  currentDate.setDate(currentDate.getDate() - 1);
   if (!date) {
     $('#addStatus').html("<p>Please enter a date.</p>");
     return false;
@@ -166,15 +166,15 @@ function checkExpiryRange() {
   const bookmarkCounter = localStorage["bookmarkCounter"];
   if (bookmarkCounter == 0) {
     localStorage["expiryRangeCounter"] = 0;
-    chrome.browserAction.setBadgeText({text: ''});
+    chrome.browserAction.setBadgeText({ text: '' });
   }
   else {
     let expiryDate = new Date();
-    expiryDate.setDate(expiryDate.getDate()+Number(localStorage["notificationRange"]));
-    for (let i=0; i<bookmarkCounter; i++) {
-      const date = parseIntoBigDate(localStorage["date"+i]);
+    expiryDate.setDate(expiryDate.getDate() + Number(localStorage["notificationRange"]));
+    for (let i = 0; i < bookmarkCounter; i++) {
+      const date = parseIntoBigDate(localStorage["date" + i]);
       if (expiryDate > date) {
-        localStorage["expiryRangeCounter"] = parseInt(localStorage["expiryRangeCounter"])+1;
+        localStorage["expiryRangeCounter"] = parseInt(localStorage["expiryRangeCounter"]) + 1;
         incrementNotifications();
       }
     }
@@ -183,16 +183,16 @@ function checkExpiryRange() {
 
 function incrementNotifications() {
   const ba = chrome.browserAction;
-  ba.setBadgeBackgroundColor({color: '#9991BF'});
-  ba.setBadgeText({text: localStorage["expiryRangeCounter"]});
+  ba.setBadgeBackgroundColor({ color: '#9991BF' });
+  ba.setBadgeText({ text: localStorage["expiryRangeCounter"] });
 }
 
 function parseIntoBigDate(date) {
-  return new Date(Number(date.substring(0,4)), Number(date.substring(5,7))-1, Number(date.substring(8,10)));
+  return new Date(Number(date.substring(0, 4)), Number(date.substring(5, 7)) - 1, Number(date.substring(8, 10)));
 }
 
 function parseIntoSmallDate(date) {
-  let month = date.getMonth()+1;
+  let month = date.getMonth() + 1;
   if (month < 10) {
     month = "0" + month;
   }
