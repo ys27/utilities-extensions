c45424c230543f3f700188be5a73907f48f463ad
Andrew An <anandrew1995@gmail.com>
2022-07-26 23:19:35 -0400

    Update to Manifest V3, remove youtube scroll

--- Main/index.js
+++ Main/index.js
@@ -12,22 +12,3 @@ $("#CheckSome").on("click", function () {
 $("#UpdateURL").on("click", function () {
   window.location = "/UpdateURL/index.html";
 });
-
-// YoutubeSpaceDown
-if (!localStorage.getItem("YoutubeSpaceDown::enabled")) {
-  localStorage.setItem("YoutubeSpaceDown::enabled", "true");
-  chrome.runtime.sendMessage({ "YoutubeSpaceDown::enabled": "true" });
-}
-$("#youtube-switch")
-  .prop("checked", localStorage.getItem("YoutubeSpaceDown::enabled") === "true")
-  .change();
-
-$("#youtube-switch").change(function () {
-  const youtubeSpaceDownEnabled = $(this).prop("checked").toString();
-  localStorage.setItem("YoutubeSpaceDown::enabled", youtubeSpaceDownEnabled);
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, {
-      "YoutubeSpaceDown::enabled": youtubeSpaceDownEnabled,
-    });
-  });
-});
--- README.md
+++ README.md
@@ -12,11 +12,6 @@ Notifies you when you should take a look at a bookmark again by setting an expir
 
 Simplifies the checking of any text on any public domain.
 
-## YoutubeSpaceDown
+## UpdateURL
 
-Re-enables Space to scroll down on Youtube (excludes video pages).
-Shift+Space to scroll upwards.
-
-## Emphisitize
-
-Re-colours visited links to a more noticeable colour.
+Change parts of the URL in the current tab or all tabs.
